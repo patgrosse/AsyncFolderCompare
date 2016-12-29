f9b40778ac4ec0663c3422f9af703172be4623aa
Patrick Grosse <patrick.grosse@uni-muenster.de>
2016-12-29 14:01:41 +0100

    Maven fixes

--- src/main/java/de/patgrosse/asyncfoldercompare/matcher/folders/LevenshteinFolderMatcher.java
+++ src/main/java/de/patgrosse/asyncfoldercompare/matcher/folders/LevenshteinFolderMatcher.java
@@ -1,6 +1,7 @@
 package de.patgrosse.asyncfoldercompare.matcher.folders;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
